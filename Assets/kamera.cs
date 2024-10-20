784c6e1d3673447a70940cc02bd16bd0bd2ffa9b
İZZET ÖZGÜR ÖNDER <izzetozgur@gmail.com>
2024-10-21 02:14:01 +0300

    İzlenmeyen dosyalar eklendi

---
+++ Assets/kamera.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class kamera : MonoBehaviour
+{
+    public Transform takip_edilen_kup;
+    private void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, takip_edilen_kup.position, Time.deltaTime * 3.0f);
+    }
+}
